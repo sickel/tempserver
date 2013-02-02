6a3d5085101ac4cc9dd4a96e26ff258b7f53ca0f
Morten Sickel <morten@sickel.net>
2013-02-02 21:38:14 +0100

    Version 1.0.UIploaded 2nd February

--- WebServer_temp_multi.ino
+++ WebServer_temp_multi.ino
@@ -4,13 +4,20 @@
  * A web server that shows the temperatures of connected DS18B20s.
  * It may either be called directly, and will then show a basic 
  * web page
- * http://<ip>
- * Or it may be called as
- * http://<ip>/json and will then return the temperatures as a json 
- * encoded string
+ * http://192.168.0.177
+ * Or it mvay be called as
+ * http://192.168.0.177/json 
+ * and will then return the temperatures as a json encoded string
+ *
+ * The html is pretty messy, but it is on the limit of how much 
+ * text the arduino can serve.
+ *
  * (c) Morten Sickel 2013
  * Based on examples from the DallasTemperature and Ethernet
  * (ENC28J60) libraries
+ * licenced under the GPL v2 or later
+ *
+ * Version 1.0 - 2nd February 2013
  */
 
 #include <Ethernet.h>
@@ -23,16 +30,23 @@ byte ip[] = { 192,168,0,177 };
 // are no other arduinoes around
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 // maxsensors is the highest number of connected sensors
-#define MAXSENSORS 5
+#define MAXSENSORS 7
+/* It seems to work with 7 sensors, with 8 or more, the sketch is 
+   hanging -it may be possible to fit in more sensors by stripping 
+   out all unneeded code (i.e. Serial.print calls for debugging and 
+   testing or unneeded web page code.) (there are also some ways of
+   using the memory better, but I have so far not gone into that)
+*/
+ 
 // The pin that gets the signal from the sensors:
-#define ONE_WIRE_BUS 3
+#define ONE_WIRE_PIN 3
 
 // There should be no need to change anything below here ...
 
 Server server(80);
 #define TEMPERATURE_PRECISION 9
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_PIN);
 
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
@@ -56,14 +70,14 @@ void setup()
   Serial.print(ndevs, DEC);
   Serial.println(" devices.");
   // report parasite power requirements
-  Serial.print("Parasite power is: "); 
+  Serial.print("Parasite power : "); 
   if (sensors.isParasitePowerMode()) Serial.println("ON");
   else Serial.println("OFF");
 
 // Setting up the thermo sensors:
   for (int i=0;i<ndevs;i++){
     if (!sensors.getAddress(thermos[i], i)){
-      Serial.print("Unable to find address for Device"); 
+      Serial.print("Unable to find device "); 
       Serial.println(i);
     }else{
       Serial.print("Sensor OK: ");
@@ -73,7 +87,6 @@ void setup()
     }
   }
   
-
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.println("Server started");
@@ -81,16 +94,15 @@ void setup()
 
 void loop()
 { 
-  // reads and stores the temperatures
-  // this could probably be moved inside the if(client)..
-  sensors.requestTemperatures();
-  float temps[MAXSENSORS];
-  for(int i=0;i<ndevs;i++){
-    temps[i]=sensors.getTempC(thermos[i]);
-  }
-  
+  // Waits for a client to connect
   Client client = server.available();
   if (client) {
+    // reads and stores the temperatures
+    sensors.requestTemperatures();
+    float temps[MAXSENSORS];
+    for(int i=0;i<ndevs;i++){
+      temps[i]=sensors.getTempC(thermos[i]);
+    }
     char request[10];
     int i = 0;
     // an http request ends with a blank line
@@ -107,10 +119,10 @@ void loop()
         if (c == '\n' && current_line_is_blank) {
           // send a standard http response header
           client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println();
           if (strncmp("GET /json", request, 8) == 0) {
-           // use Content-Type: application/json
+            // returns json encoded data
+            client.println("Content-Type: application/json");
+            client.println();
             client.print("{\"temp\":[");
             for(i=0;i<ndevs;i++){
               client.print(temps[i]);
@@ -118,29 +130,36 @@ void loop()
                 client.print(",");
               }
             }
+            // the addresses to make it possible to identify sensors
             client.print("],\"address\":[");
             for(i=0;i<ndevs;i++){
+              client.print("\"");
               printAddress(thermos[i],client);
+              client.print("\"");
               if(i<ndevs-1){
                 client.print(",");
               }
             }
- 
+            // runtime in millisecouns since boot:
             client.print("],\"millis\":");
             client.print(millis());
             client.println("}");
           }else{         
+            client.println("Content-Type: text/html");
+            client.println();
             for(i=0;i<ndevs;i++){
               client.print("sensor ");
               client.print(i);
               client.print(" : ");
               client.print(temps[i]);
-              client.println("<br />");
+              client.println(" deg C<br />");
             }
-            client.print("<br /><br />");
-            client.println(millis()/1000);
-            client.print("<br /><a href=\"/json\">json</a>");
-            client.print("<br /><a href=\"/\">refresh</a>");
+            client.print("<hr />");
+            client.println("<a href=\"/\">refresh</a> - ");
+            client.println("<a href=\"/json\">json</a><br />");
+             // to make it clear that the page updates:
+            client.print(millis()/1000);
+            client.println(" s uptime");
           }
           break;
         }
@@ -161,7 +180,7 @@ void loop()
 
 
 
-// function to print a device address
+// function to print a device address on the serial device
 void printAddress(DeviceAddress deviceAddress)
 {
   for (uint8_t i = 0; i < 8; i++)
@@ -172,15 +191,7 @@ void printAddress(DeviceAddress deviceAddress)
   }
 }
 
-// function to print a device's resolution
-void printResolution(DeviceAddress deviceAddress)
-{
-  Serial.print("Resolution: ");
-  Serial.print(sensors.getResolution(deviceAddress));
-  Serial.println();    
-}
-
-
+// function to print a device address to the client
 void printAddress(DeviceAddress deviceAddress,Client client)
 {
   for (uint8_t i = 0; i < 8; i++)
